54973a5739bfd1e95de7dd299f55b48ee19848a9
Matkronh <122084505+Matkronh@users.noreply.github.com>
2023-01-19 11:14:09 +0000

    c

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let toDoItems = []
 const toDoListBox = document.getElementById("to-do-list-box")
-
+console.log("hi")
 const siteTitle = document.createElement("h1")
 siteTitle.textContent = "Your own personal To-Do-List"
 
@@ -9,6 +9,8 @@ toDoInputField.placeholder = ("Add to do's here")
 
 const btnAddToDo = document.createElement("button")
 btnAddToDo.textContent = ("Submit ToDo")
+btnAddToDo.setAttribute("id", "btn-add-to-do")
+const btnSubmit = document.getElementById("btn-add-to-do")
 
 toDoListBox.append(
     siteTitle,
@@ -22,3 +24,8 @@ toDoListItemsDiv.setAttribute("id", "to-do-list-items-div")
 document.body.appendChild(
     toDoListItemsDiv
 )
+
+btnSubmit.addEventListener("click", function(){
+    console.log("button is clicked")
+})
+
