4af0708c89aea039b36f33ecae0ee2a510efb2bf
Matkronh <122084505+Matkronh@users.noreply.github.com>
2023-01-19 09:48:47 +0000

    changes

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
+let toDoItems = []
+const toDoListBox = document.createElement("div")
+toDoListBox.setAttribute("id", "to-do-list-box")
 
+const siteTitle = document.createElement("h1")
+siteTitle.innerText("Your own personal To-Do-List")
