1433526e34efe20d3baaf29ad41a040e625d2ab1
Matkronh <122084505+Matkronh@users.noreply.github.com>
2023-01-19 10:09:51 +0000

    added

--- index.js
+++ index.js
@@ -2,7 +2,12 @@ let toDoItems = []
 const toDoListBox = document.getElementById("to-do-list-box")
 const siteTitle = document.createElement("h1")
 siteTitle.textContent = "Your own personal To-Do-List"
-
+const toDoInputField = document.createElement('input[type="text"]')
+toDoInputField.setAttribute("placeholder", "Type in a task...")
+const btnAddToDo = document.createElement('input[type="button"]')
+btnAddToDo.setAttribute("value", "Submit")
 toDoListBox.append(
-    siteTitle
+    siteTitle,
+    toDoInputField,
+    btnAddToDo
 )
