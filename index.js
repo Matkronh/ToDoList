37652313c8315b09ec40427e5f9efd1a607433fa
Matkronh <122084505+Matkronh@users.noreply.github.com>
2023-01-24 12:23:51 +0000

    many changes

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 let toDoItems = []
-
+/* button styling array */
 let toDoFrames = [
     "https://github.com/Matkronh/ToDoList/blob/main/imgs/backgrounsetc-starfield-1.jpg?raw=true",
     "https://github.com/Matkronh/ToDoList/blob/main/imgs/backgrounsetc-starfield-2.jpg?raw=true",
@@ -7,8 +7,6 @@ let toDoFrames = [
     "https://github.com/Matkronh/ToDoList/blob/main/imgs/backgrounsetc-starfield-4.jpg?raw=true",
     "https://github.com/Matkronh/ToDoList/blob/main/imgs/backgrounsetc-starfield-5.jpg?raw=true"
 ]
-
-let numberGoUp = 0
 
 const heroDiv = document.getElementById("hero")
 const toDoListBox = document.createElement("div")
@@ -36,8 +34,18 @@ toDoListBox.append(
     btnAddToDo
 )
 
+/* 2 ways to enter to-do items, click and enter */
+
 btnAddToDo.addEventListener ("click", addToDoFunction);
 
+textInputField.addEventListener("keyup", function(event){
+    event.preventDefault();
+    if (event.keyCode === 13){
+        addToDoFunction()
+    }
+})
+
+/* Function to create the to-do's */
 
 function addToDoFunction(){
     const textElement = textInputField.value
@@ -57,25 +65,49 @@ function addToDoFunction(){
     textInputField.value = ""
 }
 
+
+
+/* Button function to render To-Do-List items */
+
 function displayToDoFunction(textElement, currentDateStamp, currentMonthStamp, currentYearStamp, currentTimeStamp){
     const paragraphElement = document.createElement("p")
+    paragraphElement.textContent = "• " + textElement + " - " + currentDateStamp + "." + currentMonthStamp + "." + currentYearStamp
+
     const removeButton = document.createElement("button")
-    const itemDiv = document.createElement("div")
-    
     removeButton.textContent = ("Remove")
     removeButton.setAttribute("class", "button2")
-    paragraphElement.textContent = "• " + textElement + " - " + currentDateStamp + "." + currentMonthStamp + "." + currentYearStamp
-    toDoListItemsDiv.append(itemDiv)
+
+    const itemDiv = document.createElement("div")
     itemDiv.setAttribute("class", "ID3")
 
-    let bgImg = toDoFrames[Math.floor(Math.random()*5)]
+    const favouriteButton = document.createElement("i")
+    favouriteButton.setAttribute("class","fa-regular fa-star iUnfavourited")
 
-    
+    let favourited = true
+
+    toDoListItemsDiv.append(itemDiv)
+
+
+    /* Button styling */
+    let bgImg = toDoFrames[Math.floor(Math.random()*5)]
     itemDiv.style.backgroundImage = `url(${bgImg})`
     itemDiv.style.backgroundSize = "80%"
-    itemDiv.append(paragraphElement, removeButton)
+    itemDiv.append(paragraphElement, favouriteButton, removeButton)
 
-    const idForRemoval = currentTimeStamp 
+    /* Button to favourite tasks that require more urgent action, which brings the task to the top */
+    favouriteButton.addEventListener("click", function(){
+        if (favourited){
+            favouriteButton.setAttribute("class", "fa-solid fa-star iFavourited")
+            favourited = false
+            toDoListBox.append(itemDiv)
+        } else if (!favourited){
+            favouriteButton.setAttribute("class","fa-regular fa-star iUnfavourited")
+            favourited = true
+            toDoListItemsDiv.append(itemDiv)
+        }
+    })
+
+    /* Button to remove a to-do list item once finished, I did not implement a completed button since it felt redundant */
     removeButton.addEventListener("click", function(){
         paragraphElement.remove()
         removeButton.remove()
@@ -84,8 +116,6 @@ function displayToDoFunction(textElement, currentDateStamp, currentMonthStamp, c
         const indexTest = toDoItems.findIndex((value) => value.id === idForRemoval)
         toDoItems.splice(indexTest, 1)
     })
-    
-    console.log(toDoItems)
 }
 
 
